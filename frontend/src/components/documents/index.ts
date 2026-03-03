ffd6898a97d67c3e34d882b83d95aef01d7c0c14
clivia <3254822118@qq.com>
2026-03-04 01:40:59 +0800

    feat: export ppt utility functions

--- frontend/src/components/documents/index.ts
+++ frontend/src/components/documents/index.ts
@@ -1,2 +1,3 @@
 export { default as DocumentPreview } from "./DocumentPreview";
 export { default as ExcalidrawPreview } from "./previews/ExcalidrawPreview";
+export { isPptxFile, isLegacyPptFile } from "./utils";
